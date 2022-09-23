4e34eb7deabeca12632576ed5a971d645f658d96
Kavya K <95955787+Kavyakavi09@users.noreply.github.com>
2022-09-24 03:15:03 +0530

    done update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const __dirname = dirname(__filename);
 const app = express();
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://invoicybilly.netlify.app'],
+    origin: '*',
     credentials: true,
   })
 );
